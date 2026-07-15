99a9d961b71726bab33c2bea65367890cd752af8
kyletp09 <kylepham999@gmail.com>
2026-07-15 11:13:24 -0700

    Scraped most recent articals and used datetime

--- codingNewsScraper.py
+++ codingNewsScraper.py
@@ -1,5 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+from datetime import datetime, timezone, timedelta
 
-response = requests.get('https://news.mit.edu/topic/machine-learning')
+response = requests.get('https://news.mit.edu/topic/mitmachine-learning-rss.xml')
+soup = BeautifulSoup(response.text, 'xml')
 
-print(response.text)
+item_list = soup.find_all('item')
+
+publication_date = [i.pubDate.text for i in item_list]
+today = datetime.now(timezone.utc)
+
+# print(publication_date[:5])
+
+difference = today - parse(publication_date[0])
+
+if difference < timedelta(days=7):
+    print("YAY!")
